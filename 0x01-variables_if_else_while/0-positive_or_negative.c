ef91d22288f42b0e703881aacbb6ff1d1d41c8b4
CodeHunter392 <omarbenamer392@gmail.com>
2023-06-16 16:52:08 -0700

    define value of n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ * main - determine if n is positive or negative or equal to zero
+ * Description: using the main function
+ * this program returns value of n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0)
+	{
+		printf("%d is positive\n",n);
+	}
+	else if(n < 0)
+	{
+		printf("%d is negative\n",n);
+	}
+	else
+		printf("%d is zero\n",n);
+	return (0);
+}
