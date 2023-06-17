a9876fedec9ea4cd6a8de344471a8dcd6954f93c
CodeHunter392 <omarbenamer392@gmail.com>
2023-06-16 18:09:14 -0700

    update file that print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,15 +10,14 @@
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    
-    putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
 
-    return (0);
+	putchar('\n');
+
+	return (0);
 }
-
