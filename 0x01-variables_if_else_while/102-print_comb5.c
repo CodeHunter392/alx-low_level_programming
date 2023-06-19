ea8aef1e6b363c9521c2652c92f637d23888c98b
CodeHunter392 <omarbenamer392@gmail.com>
2023-06-19 15:56:23 -0700

    update file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,37 +3,45 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible different combinations of three digits
- * separated by commas and spaces
+ * Description: Prints all possible combinations of two two-digit numbers
+ * separated by a space and comma, in ascending order
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int hundredsDigit;
-	int tensDigit;
-	int onesDigit;
+    int num1;
+    int num2;
 
-	for (hundredsDigit = 0; hundredsDigit < 8; hundredsDigit++)
-	{
-		for (tensDigit = hundredsDigit + 1; tensDigit < 9; tensDigit++)
-		{
-			for (onesDigit = tensDigit + 1; onesDigit <= 9; onesDigit++)
-			{
-				putchar(hundredsDigit + '0');
-				putchar(tensDigit + '0');
-				putchar(onesDigit + '0');
+    for (num1 = 0; num1 <= 98; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 99; num2++)
+        {
+            int tens1 = num1 / 10;
+            int ones1 = num1 % 10;
+            int tens2 = num2 / 10;
+            int ones2 = num2 % 10;
 
-				if (hundredsDigit != 7 || tensDigit != 8 || onesDigit != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
+            putchar(tens1 + '0');
+            putchar(ones1 + '0');
+            putchar(' ');
+            putchar(tens2 + '0');
+            putchar(ones2 + '0');
 
-	putchar('\n');
+            if (!(num1 == 98 && num2 == 99))
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	return (0);
+    putchar('\n');
+
+    return 0;
+}
+
+void putchar(int c)
+{
+    write(1, &c, 1);
 }
