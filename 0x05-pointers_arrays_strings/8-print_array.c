68b3736e731d1254d865ef0a820a25789963a721
CodeHunter392 <omarbenamer392@gmail.com>
2023-06-26 14:25:38 -0700

    update file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-	_putchar('0' + a[i]);
+	printf("%d", a[i]);
 
 	if (i != n - 1)
 	{
-	_putchar(',');
-	_putchar(' ');
+	printf(", ");
 	}
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
